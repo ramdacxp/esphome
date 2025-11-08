499f3119804d92a0d0c8a8981623207db7d9ca3b
Michael Hoser <michael.hoser@outlook.com>
2025-11-08 13:23:28 +0100

    Framed Utopie (centered text)

---
+++ utopia.h
@@ -0,0 +1,66 @@
+#include "esphome.h"
+
+static const char *LOGTAG = "Utopia";
+
+int GetTextWidth(esphome::display::Display *it, esphome::font::Font *font, const char *buffer)
+{
+  int x1 = 0;
+  int y1 = 0;
+  int width = 0;
+  int height = 0;
+  it->get_text_bounds(0, 0, buffer, font, TextAlign::TOP_LEFT, &x1, &y1, &width, &height);
+  return width;
+}
+
+int GetNumberOfLines(std::string text)
+{
+  int lines = 1;
+  size_t pos = text.find("\n");
+  while (pos != std::string::npos)
+  {
+    lines++;
+    pos = text.find("\n", pos + 1);
+  }
+  return lines;
+}
+
+void PrintMultiline(
+    esphome::display::Display *it,
+    int x, int y,
+    esphome::font::Font *font,
+    Color color,
+    TextAlign align,
+    std::string text)
+{
+
+  size_t pos = text.find("\n");
+  if (pos == std::string::npos)
+  {
+    // no newline, pint as is
+    // ESP_LOGD(LOGTAG, "Print all: '%s'", text.c_str());
+    it->print(x, y, font, color, align, text.c_str());
+  }
+  else
+  {
+    // Print first part
+    std::string first = text.substr(0, pos);
+    // ESP_LOGD(LOGTAG, "Print first: '%s'", first.c_str());
+    it->print(x, y, font, color, align, first.c_str());
+
+    std::string remain = text.substr(pos + 1);
+    // ESP_LOGD(LOGTAG, "Remaining: '%s'", remain.c_str());
+    PrintMultiline(it, x, y + font->get_height(), font, color, align, remain);
+  }
+}
+
+void PrintCenteredMultiline(
+    esphome::display::Display *it,
+    esphome::font::Font *font,
+    Color color,
+    std::string text)
+{
+  // https://next.esphome.io/components/font/#configuration-variables
+  int fontHeight = font->get_height();
+  int lines = GetNumberOfLines(text);
+  PrintMultiline(it, 32, 32 - (lines * fontHeight) / 2, font, color, TextAlign::TOP_CENTER, text);
+}
